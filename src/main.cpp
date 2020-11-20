592e1b432cbc1468a60379f552923a0324e56b60
Maxime Chretien (MixLeNain) <mchretien@linuxmail.org>
2020-11-20 14:01:06 +0100

    Initial commit
    Signed-off-by: Maxime Chretien (MixLeNain) <mchretien@linuxmail.org>

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ *    @author  : Maxime Chretien (MixLeNain)
+ *    @mail    : mchretien@linuxmail.org
+ *    @project : ESIREM TP3
+ *    @summary : Test application of different cryptographic functions
+ *    @version : v0.1
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main () {
+
+	return 0;
+}
